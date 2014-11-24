bb048c43676ebd933c39e3a3825cfbab5bf84450
Karin <karinb@email.arizona.edu>
2014-11-23 17:04:19 -0700

    Updated js files for geolocation

--- js/eventpush.js
+++ js/eventpush.js
@@ -111,35 +111,5 @@ jQuery(document).ready(function($) {
 		}
 	}
 
-	$(document).on("pageload", function() {
-
-		function showPosition(position) {
-			var latitude = position.coords.latitude;
-			var longitude = position.coords.longitude;
-			// var latlon = position.coords.latitude + "," +
-			// position.coords.longitude;
-			console.log("Latitude: " + latitude);
-			console.log("Longitude: " + longtitude);
-
-		}
-
-		function showError(error) {
-			switch (error.code) {
-			case error.PERMISSION_DENIED:
-				console.error("User denied the request for Geolocation.")
-				break;
-			case error.POSITION_UNAVAILABLE:
-				console.error("Location information is unavailable.")
-				break;
-			case error.TIMEOUT:
-				console.error("The request to get user location timed out.")
-				break;
-			case error.UNKNOWN_ERROR:
-				console.error("An unknown error occurred.")
-				break;
-			}
-		}
-	});
-
 }); // end jQuery document ready
 
