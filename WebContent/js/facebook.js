3f0c25980d279e79e50ae4216f2d704cc23c3943
Karin <karinb@email.arizona.edu>
2014-11-27 15:37:23 -0700

    FB Login Update 1

---
+++ WebContent/js/facebook.js
@@ -0,0 +1,24 @@
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '742983259107637',
+          xfbml      : true,
+          version    : 'v2.1'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+      
+      FB.getLoginStatus(function(response) {
+    	  if (response.status === 'connected') {
+    	    console.log('Logged in.');
+    	  }
+    	  else {
+    	    FB.login();
+    	  }
+    	});
